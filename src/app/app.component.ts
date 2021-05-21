2f911afee713f623589460d1010f6f2a8e25f1c0
LaviniaStiliadou <49766851+LaviniaStiliadou@users.noreply.github.com>
2021-05-22 00:32:09 +0200

    get deployment artifact back after merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -132,4 +132,4 @@ export class AppComponent implements OnInit {
     }
     
     
-}}
+}
--- src/app/services/winery.service.ts
+++ src/app/services/winery.service.ts
@@ -81,17 +81,32 @@ export class WineryService {
                 const nodeTemplate = response.nodeTemplates[key];
                 var containsParam = false;
                 for (var j = 0; j < CustomPropsProvider.template.length; j++) {        
-                        if (nodeTemplate.name == CustomPropsProvider.template[j].name) {
+                    if (nodeTemplate.id == CustomPropsProvider.template[j].name) {
                           containsParam = true;
                         }
                 }
                 if (!containsParam) {
-                    CustomPropsProvider.template.push({value: nodeTemplate.name, name:
-                        nodeTemplate.name});
+                    CustomPropsProvider.template.push({value: nodeTemplate.id, name:
+                        nodeTemplate.id});
+                        if(nodeTemplate.deploymentArtifacts != undefined){
+                            for(var k = 0; k < nodeTemplate.deploymentArtifacts.deploymentArtifact.length; k++){
+                                let ref = nodeTemplate.deploymentArtifacts.deploymentArtifact[k].artifactRef
+                                let index = ref.indexOf("}");
+                                let temp = ref.substring(index+1);
+                                CustomPropsProvider.DA.push({value: temp, name:
+                                    temp});
+                            }
+                        }
                 }
                 if(CustomPropsProvider.template.length == 0){
-                    CustomPropsProvider.template.push({value: nodeTemplate.name, name:
-                        nodeTemplate.name});
+                    CustomPropsProvider.template.push({value: nodeTemplate.id, name:
+                        nodeTemplate.id});
+                        if(nodeTemplate.deploymentArtifacts != undefined){
+                            for(var k = 0; k < nodeTemplate.deploymentArtifacts.length; k++){
+                                CustomPropsProvider.DA.push({value: nodeTemplate.deploymentArtifacts.deploymentArtifact[k].artifactRef, name:
+                                    nodeTemplate.deploymentArtifacts.deploymentArtifact[k].artifactRef});
+                            }
+                        }
                 }
                 nodeTemplates.push(new NodeTemplate(
                     nodeTemplate.id,
